99f8924374147b235c280c870287fbc64dc6deec
Thomas Stromberg <t+git.galadriel@stromberg.org>
2026-08-22 10:06:22 -0400

    add more replicated tables

--- scripts/replica/replicated-tables.sh
+++ scripts/replica/replicated-tables.sh
@@ -3,4 +3,4 @@
 # Keep this aligned with scripts/master/freebsd-bastille.sh.
 # Consumed by the scripts that source this file, not by this file itself.
 # shellcheck disable=SC2034
-REPLICATED_TABLES='samples reports sample_locations sightings'
+REPLICATED_TABLES='samples reports sample_locations sightings claims label_events sample_location_history'
